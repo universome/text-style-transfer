eee4d2bcc3f3a523407ac07a31586fdcc3a7e718
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-02-19 20:08:37 +0300

    feat: ffn network for discriminator

---
+++ src/models.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+
+class FFN(nn.Module):
+    def __init__(self, input_size, n_hid_layers, hid_size=1024):
+        super(FFN, self).__init__()
+
+        self.n_hid_layers = n_hid_layers
+        self.hid_size = hid_size
+        self.input_size = input_size
+
+        self.nn = nn.Sequential(
+            nn.Linear(self.input_size, self.hid_size),
+            nn.ReLU(),
+            self._init_hidden(),
+            nn.Linear(self.hid_size, 1),
+            nn.Sigmoid()
+        )
+
+    def _init_hidden(self):
+        layers = []
+
+        for _ in range(self.n_hid_layers):
+            layers += [nn.Linear(self.hid_size, self.hid_size), nn.ReLU()]
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.nn(x)
