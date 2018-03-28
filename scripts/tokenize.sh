cf298a1be6859ee1d5d087c84fefb328eff061c6
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-03-28 17:32:12 +0000

    feat: take notebook apart

---
+++ scripts/tokenize.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mosesdecoder="ext-libs/mosesdecoder"
+input_file=$1
+output_file=$2
+threads=10
+
+echo "Input file: $input_file"
+echo "Output file: $output_file"
+echo "Threads: $threads"
+
+cat $input_file | \
+    $mosesdecoder/scripts/tokenizer/normalize-punctuation.perl -l ru | \
+    $mosesdecoder/scripts/tokenizer/tokenizer.perl -threads $threads -l ru > \
+    $output_file
+
