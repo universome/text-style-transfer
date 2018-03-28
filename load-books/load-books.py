8895121fdeba1e83c270f3483e3a3f58f6ab90f9
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-03-28 21:09:57 +0300

    feat: load books from catalogs

--- load-books/load-books.py
+++ load-books/load-books.py
@@ -18,10 +18,15 @@ AUTHOR_BOOKS_URL = '{}/authors/{{}}/books.json'.format(API_URL)
 BOOK_SEARCH_QUERY = 'https://aldebaran.ru/pages/biblio_search/?q={}'
 BOOKS_HOST_URL = 'https://aldebaran.ru'
 HEADERS = {
-    'Host': 'aldebaran.ru',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+    'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+    'Cookie': '_ga=GA1.2.276499901.1522103961; _gid=GA1.2.2023687985.1522103961; _ym_uid=1522103961949157268; _ym_isad=2; _ym_visorc_27736506=w; SID=5kdva0dwbef59nfb7we7746o2n1z6o7n; _ym_visorc_2199583=b; __utma=267605726.276499901.1522103961.1522104106.1522104106.1; __utmc=267605726; __utmz=267605726.1522104106.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmb=267605726.2.10.1522104106',
 }
 
 
@@ -30,6 +35,8 @@ def main(cmd):
         load_authors_ids()
     elif cmd == 'load_books_lists':
         load_books_lists()
+    elif cmd == 'load_books_from_catalogs':
+        load_books_from_catalogs()
     elif cmd == 'load_books_page_urls':
         load_books_page_urls()
     elif cmd == 'download_books':
@@ -38,10 +45,15 @@ def main(cmd):
         unzip_books()
     elif cmd == 'convert_books_to_txt':
         convert_books_to_txt()
-    elif cmd == 'all':
+    elif cmd == 'crawl-from-api':
         load_authors_ids()
         load_books_lists()
         load_books_page_urls()
+        download_books()
+        unzip_books()
+        convert_books_to_txt()
+    elif 'crawl-from-catalogs':
+        load_books_from_catalogs()
         download_books()
         unzip_books()
         convert_books_to_txt()
@@ -139,26 +151,33 @@ def load_books_page_urls():
             json.dump(books_urls, f)
 
 
-# def load_books_file_urls():
-#     books_urls = json.load(open('load-books/books-page-urls.json', encoding='utf-8'))
-#     print('Num books to retrieve:', len(books_urls))
+def load_books_from_catalogs():
+    catalogs_urls = json.load(open('load-books/catalogs_urls.json', encoding='utf-8'))
+    catalogs_urls = catalogs_urls
+    books_urls = []
 
-#     for url in tqdm(books_urls):
-#         time.sleep(5)
-#         doc = pq(url=url, headers=HEADERS)
+    print('Num catalogs to download:', len(catalogs_urls))
 
-#         if len(doc('.wrapper .block_mixed .bm1 .item_info.border_bottom')) != 1: continue
-#         if len(doc('.wrapper .block_mixed .bm1 .item_info.border_bottom a:nth-child(2)')) != 1: continue
+    for catalog_url in tqdm(catalogs_urls):
+        time.sleep(3)
+        print('Downaloding catalog:', catalog_url)
+        doc = pq(url=catalog_url, headers=HEADERS)
 
-#         el = doc('.wrapper .block_mixed .bm1 .item_info.border_bottom a:nth-child(2)')
-#         if el.text() != 'html.zip':
-#             print('Found unusual format:', el.text(), 'for url:', url)
-#             continue
+        if 'робот' in doc.text():
+            print('We are banned :(')
+            continue
 
-#         books_file_urs.append(BOOKS_HOST_URL + el.attrib['href'])
+        links = doc('.book_intro a')
 
-#         with open('load-books/books-file-urls.json', 'w', encoding='utf-8') as f:
-#             json.dump(books_file_urs, f)
+        if len(links) != 40:
+            print('Catalog has bad format:', catalog_url)
+            continue
+
+        hrefs = [BOOKS_HOST_URL + l.attrib['href'] for l in links]
+        books_urls.extend(hrefs)
+
+    with open('load-books/books-page-urls.json', 'w', encoding='utf-8') as f:
+        json.dump(books_urls, f)
 
 
 def download_books():
@@ -168,7 +187,7 @@ def download_books():
     if not os.path.exists('load-books/archives'): os.mkdir('load-books/archives')
 
     for page_url in tqdm(books_urls):
-        time.sleep(15)
+        time.sleep(60)
         download_url = page_url + 'download.html.zip'
         print('Downloading', download_url)
 
@@ -182,7 +201,7 @@ def download_books():
 
 def unzip_books():
     if not os.path.exists('load-books/html'): os.mkdir('load-books/html')
-    archives = [a for a in os.listdir('load-books/archives') if b[-4:] == '.zip']
+    archives = [b for b in os.listdir('load-books/archives') if b[-4:] == '.zip']
 
     for archive in archives:
         try:
@@ -204,7 +223,7 @@ def convert_books_to_txt():
     if not os.path.exists('load-books/txt'): os.mkdir('load-books/txt')
     book_names = [b[:-5] for b in os.listdir('load-books/html') if b[-5:] == '.html']
 
-    for book_name in book_names:
+    for book_name in tqdm(book_names):
         in_book_path = 'load-books/html/' + book_name + '.html'
         out_book_path = 'load-books/txt/' + book_name + '.txt'
 
@@ -221,5 +240,5 @@ def convert_books_to_txt():
 
 if __name__ == '__main__':
     cmd = sys.argv[1]
-    print('Command to run:', cmd)
+    print('Running command:', cmd)
     main(cmd)
