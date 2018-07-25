e0d0a76e7acfd6fd7d10372c324e82b90c4c9b66
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-07-25 11:33:28 +0300

    feat: pass language as argument ot scripts

--- scripts/split.sh
+++ scripts/split.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 mosesdecoder="ext-libs/mosesdecoder"
-threads=20
 input_file=$1
 output_file=$2
+lang=${3:-ru}
+threads=${4:-20}
 
 echo "Splitting $input_file into $output_file"
+echo "Language is $lang, threads is $threads"
 
-$mosesdecoder/scripts/ems/support/split-sentences.perl -l ru -threads $threads \
-    < $input_file > $output_file
-
+$mosesdecoder/scripts/ems/support/split-sentences.perl \
+    -l $lang -threads $threads < $input_file > $output_file
--- scripts/tokenize.sh
+++ scripts/tokenize.sh
@@ -3,14 +3,15 @@
 mosesdecoder="ext-libs/mosesdecoder"
 input_file=$1
 output_file=$2
-threads=20
+lang=${3:-ru}
+threads=${4:-20}
 
 echo "Input file: $input_file"
 echo "Output file: $output_file"
 echo "Threads: $threads"
+echo "Lang: $lang"
 
 cat $input_file | \
-    $mosesdecoder/scripts/tokenizer/normalize-punctuation.perl -l ru | \
-    $mosesdecoder/scripts/tokenizer/tokenizer.perl -threads $threads -l ru > \
+    $mosesdecoder/scripts/tokenizer/normalize-punctuation.perl -l $lang | \
+    $mosesdecoder/scripts/tokenizer/tokenizer.perl -threads $threads -l $lang > \
     $output_file
-
