d79ad1aa1839dbf0aec004971d824270c8563fe5
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-08-27 17:43:21 +0300

    feat: prepare data for word filling

--- scripts/datasets-preprocessing.py
+++ scripts/datasets-preprocessing.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+from typing import List
+
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
 
 
 def prepare_subs_for_open_nmt(data_path):
+    print('Preparing subs for open-nmt')
     print('Reading data...')
-    data = open(data_path).read().splitlines()
+    data = read_corpus(data_path)
     src = data[:-1]
     trg = data[1:]
 
@@ -23,7 +27,40 @@ def prepare_subs_for_open_nmt(data_path):
     print('Done!')
 
 
-def save_corpus(corpus, path):
+def prepare_word_filling(corpus_data_path: str, words_data_path: str):
+    print('Preparing word filling')
+
+    DROP = '__DROP__'
+
+    print('Reading data...')
+    lines = read_corpus(corpus_data_path)
+    words = set(read_corpus(words_data_path))
+    src, trg = [], []
+
+    for s in tqdm(lines):
+        tokens = s.split()
+
+        for i, t in enumerate(tokens):
+            if not (t in words): continue
+
+            src.append(' '.join(tokens[:i] + [DROP] + tokens[i+1:]))
+            trg.append(t)
+
+    print('Saving...')
+    save_corpus(src, 'data/generated/classics-dropped-words.src')
+    save_corpus(trg, 'data/generated/classics-dropped-words.trg')
+
+    print('Done!')
+
+
+def read_corpus(data_path: str) -> List[str]:
+    with open(data_path) as f:
+        lines = f.read().splitlines()
+
+    return lines
+
+
+def save_corpus(corpus: List[str], path: str):
     with open(path, 'w') as f:
         for line in corpus:
             f.write(line + '\n')
@@ -32,9 +69,11 @@ def save_corpus(corpus, path):
 def main(cmd, *args):
     if cmd == 'subs-open-nmt':
         prepare_subs_for_open_nmt(*args)
+    elif cmd == 'word-filling':
+        prepare_word_filling(*args)
     else:
         raise NotImplementedError
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], *sys.argv[2:])
