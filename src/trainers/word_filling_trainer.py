a7b66394823de403b933a9e70cd85ac9d4ef7097
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-08-29 11:55:05 +0300

    fix: make text field pickable

--- src/trainers/word_filling_trainer.py
+++ src/trainers/word_filling_trainer.py
@@ -13,11 +13,13 @@ from firelab import BaseTrainer
 from firelab.utils.training_utils import cudable
 from sklearn.model_selection import train_test_split
 
+from src.models import FFN
 from src.models.dissonet import RNNEncoder, RNNDecoder
 from src.losses.ce_without_pads import cross_entropy_without_pads
 from src.losses.bleu import compute_bleu_for_sents
 from src.inference import inference
-from src.utils.data_utils import itos_many
+from src.utils.data_utils import itos_many, char_tokenize
+from src.morph import morph_chars_idx, MORPHS_SIZE
 
 
 class WordFillingTrainer(BaseTrainer):
@@ -33,7 +35,7 @@ class WordFillingTrainer(BaseTrainer):
         with open(data_path_src) as f: src = f.read().splitlines()
         with open(data_path_trg) as f: trg = f.read().splitlines()
 
-        text = Field(init_token='<bos>', eos_token='<eos>', batch_first=True, tokenize=lambda s: list(s))
+        text = Field(init_token='<bos>', eos_token='<eos>', batch_first=True, tokenize=char_tokenize)
         fields = [('src', text), ('trg', text)]
         examples = [Example.fromlist([m,o], fields) for m,o in zip(src, trg)]
         train_exs, val_exs = train_test_split(examples,
@@ -42,7 +44,7 @@ class WordFillingTrainer(BaseTrainer):
         self.train_ds, self.val_ds = Dataset(train_exs, fields), Dataset(val_exs, fields)
         text.build_vocab(self.train_ds, max_size=self.config.hp.get('max_vocab_size'))
 
-        self.vocab = text.vocab
+        self.text = text
         batch_size = self.config.hp.batch_size
         self.train_dataloader = data.BucketIterator(self.train_ds, batch_size, repeat=False)
         self.val_dataloader = data.BucketIterator(self.val_ds, batch_size, repeat=False)
@@ -50,16 +52,18 @@ class WordFillingTrainer(BaseTrainer):
     def init_models(self):
         size = self.config.hp.model_size
 
-        self.encoder = cudable(RNNEncoder(size, size, self.vocab))
-        self.decoder = cudable(RNNDecoder(size, size, self.vocab))
+        self.encoder = cudable(RNNEncoder(size, size, self.text.vocab))
+        self.merge_z = cudable(FFN([size + MORPHS_SIZE, size, size]))
+        self.decoder = cudable(RNNDecoder(size, size, self.text.vocab))
 
     def init_criterions(self):
-        self.criterion = cross_entropy_without_pads(self.vocab)
+        self.criterion = cross_entropy_without_pads(self.text.vocab)
 
     def init_optimizers(self):
         self.optim = Adam(chain(
             self.encoder.parameters(),
-            self.decoder.parameters()
+            self.merge_z.parameters(),
+            self.decoder.parameters(),
         ), lr=self.config.hp.lr)
 
     def train_on_batch(self, batch):
@@ -75,9 +79,14 @@ class WordFillingTrainer(BaseTrainer):
         batch.src = cudable(batch.src)
         batch.trg = cudable(batch.trg)
 
+        morphs = morph_chars_idx(batch.trg, self.text.vocab)
+        morphs = cudable(torch.from_numpy(morphs).float())
+
         z = self.encoder(batch.src)
+        z = self.merge_z(torch.cat([z, morphs], dim=1))
+
         preds = self.decoder(z, batch.trg[:, :-1])
-        loss = self.criterion(preds.view(-1, len(self.vocab)), batch.trg[:, 1:].contiguous().view(-1))
+        loss = self.criterion(preds.view(-1, len(self.text.vocab)), batch.trg[:, 1:].contiguous().view(-1))
 
         return loss
 
@@ -100,17 +109,22 @@ class WordFillingTrainer(BaseTrainer):
 
             # Trying to reconstruct from first 3 letters
             first_chars_embs = self.decoder.embed(batch.trg[:, :4])
+
+            morphs = morph_chars_idx(batch.trg, self.text.vocab)
+            morphs = cudable(torch.from_numpy(morphs).float())
+
             z = self.encoder(batch.src)
+            z = self.merge_z(torch.cat([z, morphs], dim=1))
             z = self.decoder.gru(first_chars_embs, z.unsqueeze(0))[1].squeeze()
-            out = inference(self.decoder, z, self.vocab, max_len=30)
+            out = inference(self.decoder, z, self.text.vocab, max_len=30)
 
             first_chars = batch.trg[:, :4].cpu().numpy().tolist()
             results = [s + p for s,p in zip(first_chars, out)]
-            results = itos_many(results, self.vocab, sep='')
+            results = itos_many(results, self.text.vocab, sep='')
 
             generated.extend(results)
-            sources.extend(itos_many(batch.src, self.vocab, sep=''))
-            gold.extend(itos_many(batch.trg, self.vocab, sep=''))
+            sources.extend(itos_many(batch.src, self.text.vocab, sep=''))
+            gold.extend(itos_many(batch.trg, self.text.vocab, sep=''))
 
         # Let's try to measure BLEU scores (not valid, but at least something)
         sents_generated = [' '.join(list(s[4:])) for s in generated]
