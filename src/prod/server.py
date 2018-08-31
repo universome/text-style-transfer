09f2b5b1ec6b31c36050402d4bc2279685aef80a
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-08-31 14:45:43 +0300

    feat: add more mocked data

--- src/prod/server.py
+++ src/prod/server.py
@@ -1,12 +1,13 @@
 "Server for running the model"
 
 import json
+import random
 
 from flask import Flask, request
 from flask_restful import Resource, Api, output_json
 
-from model import predict
-
+# from model import predict
+from news import retrieve_random_dialog
 
 class UnicodeApi(Api):
     def __init__(self, *args, **kwargs):
@@ -22,37 +23,40 @@ class UnicodeApi(Api):
 app = Flask(__name__)
 api = UnicodeApi(app)
 
-class Style(Resource):
-    def post(self):
-        if request.json is None:
-            return {'error': 'You should send me json data!'}, 400
+# class Style(Resource):
+#     def post(self):
+#         if request.json is None:
+#             return {'error': 'You should send me json data!'}, 400
 
-        if not 'sentences' in request.json:
-            return {'error': 'Your json request should inlcude `sentences`'}, 400
+#         if not 'sentences' in request.json:
+#             return {'error': 'Your json request should inlcude `sentences`'}, 400
 
-        try:
-            # print(predict(request.json['sentences']))
-            return {'result': predict(request.json['sentences'])}
-        except Exception as e:
-            print('Error occured:', e)
+#         try:
+#             # print(predict(request.json['sentences']))
+#             return {'result': predict(request.json['sentences'])}
+#         except Exception as e:
+#             print('Error occured:', e)
 
-            return {'error': 'Something went wrong'}, 500
+#             return {'error': 'Something went wrong'}, 500
 
 
 class Dialog(Resource):
     def get(self):
-        return {'result': [
-            {'speaker': 'Bes', 'text': 'Привет, меня зовут Бес.', 'sentiment': 0.7},
-            {'speaker': 'Borgy', 'text': 'Ну и имечко! Я — Берги.', 'sentiment': 0.3},
-            {'speaker': 'Bes', 'text': 'Звучит не лучше.', 'sentiment': 0.1},
-            {'speaker': 'Bes', 'text': 'В чем смысл жизни, Берги?', 'sentiment': 0.4},
-            {'speaker': 'Bergy', 'text': 'Свисать с потолка и разговаривать с хирургической лампой.', 'sentiment': 0.4},
-            {'speaker': 'Bes', 'text': 'Подожди. У меня такое чувство, будто нас подслушивают.', 'sentiment': 0.3},
-            {'speaker': 'Bes', 'text': 'Жаль, что я не могу ни видеть, ни слышать.', 'sentiment': 0.1}
-        ]}
+        if random.random() > 0.75:
+            return {'result': [
+                {'speaker': 'Bes', 'text': 'Привет, меня зовут Бес.'},
+                {'speaker': 'Borgy', 'text': 'Ну и имечко! Я — Берги.'},
+                {'speaker': 'Bes', 'text': 'Звучит не лучше.'},
+                {'speaker': 'Bes', 'text': 'В чем смысл жизни, Берги?'},
+                {'speaker': 'Bergy', 'text': 'Свисать с потолка и разговаривать с хирургической лампой.'},
+                {'speaker': 'Bes', 'text': 'Подожди. У меня такое чувство, будто нас подслушивают.'},
+                {'speaker': 'Bes', 'text': 'Жаль, что я не могу ни видеть, ни слышать.'}
+            ]}
+        else:
+            return {'result': retrieve_random_dialog()}
 
 
-api.add_resource(Style, '/style')
+# api.add_resource(Style, '/style')
 api.add_resource(Dialog, '/dialog')
 
 if __name__ == '__main__':
