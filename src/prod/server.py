d44a9436e0052398cb112f516d195d89259eeb60
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-09-07 16:18:55 +0300

    feat: deploying tf LM

--- src/prod/server.py
+++ src/prod/server.py
@@ -9,6 +9,7 @@ from flask_restful import Resource, Api, output_json
 
 from model import predict
 from news import retrieve_random_dialog
+from lm import continue_dialog
 
 class UnicodeApi(Api):
     def __init__(self, *args, **kwargs):
@@ -55,9 +56,36 @@ class Dialog(Resource):
         else:
             return {'result': retrieve_random_dialog()}
 
+    def post(self):
+        if request.json is None:
+            return {'error': 'You should send me json data!'}, 400
+
+        if not 'dialog_requests' in request.json:
+            return {'error': 'Your json request should inlcude `dialog_requests`'}, 400
+
+        try:
+            dialogs = []
+
+            for req in request.json['dialog_requests']:
+                if not 'sentence' in req or not 'n_lines' in req:
+                    return {'error': 'Each dialog request should include `sentence` (str) and `n_lines` (int)'}, 400
+
+                if not type(req['sentence']) is str: return {'error': '`sentence` must be a string!'}, 400
+                if not type(req['n_lines']) is int: return {'error': '`n_lines` must be an integer!'}, 400
+                if not 0 < req['n_lines'] <= 100: return {'error': '`n_lines` must be between 1 and 100!'}, 400
+
+                continuation = continue_dialog(req['sentence'], req['n_lines'])
+                dialog = [req['sentence']] + continuation
+                dialogs.append(dialog)
+
+            return {'result': dialogs}
+        except Exception:
+            traceback.print_exc()
+
+            return {'error': 'Something went wrong'}, 500
 
 api.add_resource(Style, '/style')
 api.add_resource(Dialog, '/dialog')
 
 if __name__ == '__main__':
-    app.run(port=10101, host='0.0.0.0')
+    app.run(port=10101, host='0.0.0.0', debug=True)
